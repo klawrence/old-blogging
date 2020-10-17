f0b06ac45b865f8c44112d8698b682950e38bc60
kevin <kevin@diamond-sky.com>
2020-10-17 06:43:52 +0100

    Serve a post as JSON

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: :show
+
+  private
+  def set_post
+    @post = Post.find params[:id]
   end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
+  setup do
+    @post = Post.create! title: 'The title', body: 'The body.'
+  end
+
+  test 'get the posts index' do
     get posts_url
     assert_response :success
   end
+
+  test 'fetch a post as json' do
+    get post_url(@post, format: :json)
+    assert_response :success
+    json = JSON.parse response.body, symbolize_names: true
+
+    post_json = json #json[:posts][0]
+    assert_equal @post.id, post_json[:id]
+    assert_equal 'The title', post_json[:title]
+    assert_equal 'The body.', post_json[:body]
+  end
+
 end
