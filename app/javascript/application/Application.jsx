53ee695715bfdd9a1343746ca7a4056862fcbc78
kevin <kevin@diamond-sky.com>
2020-10-18 16:32:14 +0100

    extract a form for posts

--- app/javascript/application/Application.jsx
+++ app/javascript/application/Application.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import Post from 'posts/Post'
+import {Form} from '../posts/Form'
 
-export function Application(_props) {
+export function Application() {
   return <div id='application'>
     <div id='header' className='header'>
       <h1 className='site-name'>Blogging</h1>
     </div>
 
     <div id='main'>
-      <form>
-        <input type='text' name='title' />
-        <textarea name='body' />
-        <div className='actions'>
-          <input type='submit' className='button' />
-        </div>
-      </form>
+      <Form />
       <Post id={1}/>
     </div>
   </div>
