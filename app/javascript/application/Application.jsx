3abdd98711418ac8e00e6faa4eaabd1d9b426f0c
kevin <kevin@diamond-sky.com>
2020-10-18 16:02:57 +0100

    add a form for new posts

--- app/javascript/application/Application.jsx
+++ app/javascript/application/Application.jsx
@@ -4,6 +4,13 @@ import Post from 'posts/Post'
 export function Application(_props) {
   return <div id='application'>
     <h1 className='site-name'>Blogging</h1>
+    <form>
+      <input type='text' name='title' />
+      <textarea name='body' />
+      <input type='submit' />
+    </form>
     <Post id={1} />
   </div>
 }
+
+
